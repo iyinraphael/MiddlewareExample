ce5cd5796bc5eb1b4ba6763804a55d3a33df145e
Raphael Iyin <RaphaelLyin@MacBook-Pro.lan>
2023-01-22 01:08:22 -0500

    undertanding middleeare and everything between

---
+++ MiddlewareExample/Program.cs
@@ -0,0 +1,56 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// To chanin multiple request delegates in our code, we can use the Use method
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+
+app.Map("/usingmapbranch", builder =>
+{
+    builder.Use(async(context, next) =>
+    {
+        Console.WriteLine("Map branch logic in the Use method before next delagte");
+        await next.Invoke();
+        Console.WriteLine("Map branch logic in the Use method after the next delegate");
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch respose to the client in the Run method");
+        await context.Response.WriteAsync("Hello from the map branch");
+    });
+});
+
+app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+{
+    builder.Run(async context =>
+    {
+        await context.Response.WriteAsync("Hello from th MapWhen branch");
+    });
+});
+// Run method adds a terminal component to the app
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Hello from the middleware component side of things!");
+});
+
+app.MapControllers();
+
+app.Run();
+
